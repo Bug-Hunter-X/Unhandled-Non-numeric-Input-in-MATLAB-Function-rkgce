eec11d1306dd2c788e10cd59af21e27b20d01571
BugHunter-X <askzenai@gmail.com>
2025-01-31 07:56:04 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,12 @@
+function result = myFunction(input)
+  % Some code here
+  if input > 10
+    result = input * 2; 
+  else
+    result = input + 5; 
+  end
+end
+
+%Example usage
+input = 5;
+output = myFunction(input);
